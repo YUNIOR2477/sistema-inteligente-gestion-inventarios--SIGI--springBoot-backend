621bf0c5f4b8fe2d6a9a22f0387f3108f19420df
YUNIOR2477 <yu3217753777@gmail.com>
2026-03-27 16:09:12 -0500

    deploy in render

--- src/main/java/com/sigi/persistence/repository/UserRepository.java
+++ src/main/java/com/sigi/persistence/repository/UserRepository.java
@@ -2,9 +2,7 @@ package com.sigi.persistence.repository;
 
 import com.sigi.persistence.entity.User;
 import com.sigi.persistence.enums.RoleList;
-import jakarta.validation.constraints.Size;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/test/java/com/sigi/SigiBackendApplicationTests.java
+++ src/test/java/com/sigi/SigiBackendApplicationTests.java
@@ -2,12 +2,11 @@ package com.sigi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SigiBackendApplicationTests {
-
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
